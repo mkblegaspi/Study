2d85228bede3de29473e09be813b00b54c015284
mkblegaspi <mklegaspi@hotmail.com>
2024-02-29 08:21:06 +0800

    Add source code sample of global scope.

---
+++ C++/C++ in Action/global_scope.cpp
@@ -0,0 +1,21 @@
+#icnlude <iostream>
+
+class World
+{
+public:
+  World ()
+  {
+    std::cout << "Hello!" << std::endl;
+  }
+  ~World ()
+  {
+    std::cout << "Good bye!" << std::endl;
+  }
+};
+
+World TheWorld;
+
+int main ()
+{
+  return 0;
+}
