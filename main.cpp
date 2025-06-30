9c85cb2e9eaa769839800237803f483ed2dfea4a
mkblegaspi <mklegaspi@hotmail.com>
2025-07-01 06:23:45 +0800

    July 1 6:23

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
 // Local Variables and Function Arguments
 // Local variables
+// Local variables exist inside a scope
+//  - A scope is defined by a pair of braces {}
+// A local vaiable comes into existence where it is defined
+//  - Memory is automatically allocated for it on the program's stack
+//  - It will then be initialized
+//  - If we do not specify an initial value, it is "default initialized"
+// It is destroyed at the end of its scope
+/*
+  {               // Start of a scope
+    int i;        // Allocate memory for i, defaujlt initialize it
+    ...           // Code that uses i
+  }               // End od scope - release memory used by i isn't longer "in scope"
+*/
